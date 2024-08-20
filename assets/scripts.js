5e685063e159d13255c0b8ef6bad30a12242bb29
lauraheritiertrenders <150356588+lauraheritiertrenders@users.noreply.github.com>
2024-08-19 23:08:33 -0300

    ajustes

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     const swiper = new Swiper('.swiper', {
         slidesPerView: 4,
-        spaceBetween: 10
+        spaceBetween: 10,
+        loop: true,
+        autoPlay: true,
       });
 });
